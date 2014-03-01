1d68b978570cf59ad240cfd75ca61244dcd288c8
TripleHelixProgramming@gmail.com <TripleHelixProgramming@gmail.com@a619677c-f152-d5e7-c405-8ab0a5463c6c>
2014-03-01 19:15:54 +0000

    Corrected repository location

---
+++ branches/Zeta Practice3.1.14/Zeta Practice/src/edu/wpi/first/wpilibj/templates/commands/lights/TurnLightsOnCommand.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands.lights;
+
+import edu.wpi.first.wpilibj.templates.commands.CommandBase;
+
+/**
+ *
+ * @author robotics
+ */
+public class TurnLightsOnCommand extends CommandBase {
+    
+    public TurnLightsOnCommand() {
+        requires(lights);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+        lights.turnOn(true);
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
---
+++ branches/Zeta Practice3.1.14/Zeta Practice/src/edu/wpi/first/wpilibj/templates/subsystems/PickUp.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+import edu.wpi.first.wpilibj.templates.commands.pickup.PickUpDeploy;
+import edu.wpi.first.wpilibj.templates.util.RollingAverager;
+
+/**
+ *
+ * @author robotics
+ */
+public class PickUp extends Subsystem {
+    
+    public static final boolean DEPLOY = true;
+    public static final boolean RETRACT = false;
+    
+    private Solenoid solenoid1 = new Solenoid(1, RobotMap.pickUpSolenoid1);
+    private Solenoid solenoid2 = new Solenoid(1, RobotMap.pickUpSolenoid2);
+    
+    private SpeedController upperPickUp = new Talon(RobotMap.upperPickUpRoller);
+    
+    private double powerLevel;
+    
+    private RollingAverager pickUpAverager = new RollingAverager(20, 0);
+    
+    public void initDefaultCommand() {
+        setDefaultCommand(new PickUpDeploy(PickUp.RETRACT, 0));
+    }
+    
+    public void setRollerSpeed(double power){
+        powerLevel = power;
+    }
+    
+    public void deployArm(boolean deploy){
+       solenoid1.set(!deploy);
+       solenoid2.set(deploy);
+    }
+    
+    public void runRoller(){
+        pickUpAverager.addValue(powerLevel);
+        upperPickUp.set(pickUpAverager.getAverage());
+    }
+}
