15f6bdd9bcb4d9db90dce5a5b37dd0241490c6cd
triplehelixprogramming <TripleHelixProgramming@gmail.com>
2014-05-08 22:44:32 +0000

    Final code from Championship including goalie pole

---
+++ Zeta_ChampionshipQuick/src/edu/wpi/first/wpilibj/templates/commands/shooter/SetShooterPosition.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands.shooter;
+
+import edu.wpi.first.wpilibj.templates.commands.CommandBase;
+
+/**
+ *
+ * @author robotics
+ */
+public class SetShooterPosition extends CommandBase {
+    public boolean shooterPosition;
+    public SetShooterPosition(boolean shooterPosition) {
+        requires(shooter);
+        this.shooterPosition = shooterPosition;
+    }
+
+    protected void initialize() {
+//        System.out.println("Setting Shooter Position ");
+    }
+    
+    protected void execute() {
+        shooter.primeShooter(shooterPosition);
+    }
+
+    protected boolean isFinished() {
+        return true;
+    }
+
+    protected void end() {
+//        System.out.println("Shooter Position Set");
+    }
+
+    protected void interrupted() {
+    }
+}
---
+++ Zeta_ChampionshipQuick/src/edu/wpi/first/wpilibj/templates/commands/shooter/ShootSeries.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands.shooter;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import edu.wpi.first.wpilibj.templates.commands.pickup.PickUpDeploy;
+import edu.wpi.first.wpilibj.templates.commands.pickup.ResetArmCommand;
+import edu.wpi.first.wpilibj.templates.subsystems.PickUp;
+import edu.wpi.first.wpilibj.templates.subsystems.Shooter;
+
+
+public class ShootSeries extends CommandGroup {
+    
+    public ShootSeries() {
+        addParallel(new PickUpDeploy(PickUp.DEPLOY, 0, PickUp.CLOSE));
+        addSequential(new WaitCommand(0.25));
+        addSequential(new SetShooterPosition(Shooter.FIRE));
+        addSequential(new WaitCommand(0.2));
+        addSequential(new SetShooterPosition(Shooter.PRIME));
+        addSequential(new WaitCommand(0.1));
+        addParallel(new ResetArmCommand());    }
+}
